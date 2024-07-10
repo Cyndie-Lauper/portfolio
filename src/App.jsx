f1f31758f787cd4f8d7f49cf40166ba52fdbf9eb
Minh <elevenine00@gmail.com>
2024-07-11 00:17:07 +0700

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,48 @@ import Success from "./pages/SuccessPage";
 import { useEffect, useState } from "react";
 import { AnimatePresence } from "framer-motion";
 import Preloader from "./components/Preloader/Preloader";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
+import { Helmet } from "react-helmet";
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-      setTimeout(() => {
-        setIsLoading(false);
-        window.scrollTo(0, 0);
-      }, 2000);
-    });
+    setTimeout(() => {
+      setIsLoading(false);
+      window.scrollTo(0, 0);
+    }, 2000);
+  });
 
   return (
     <>
       <AnimatePresence mode="wait">
         {isLoading && <Preloader />}
       </AnimatePresence>
+      <div>
+        <Helmet>
+          <title>Minh — Frontend Developer & Designer</title>
+          <meta
+            name="description"
+            content="I create elevating digital experiences that inspire and connect with people through design and development"
+          />
+          <meta name="keywords" content="Portfolio" />
+          <meta name="author" content="Minh" />
+          <meta
+            property="og:title"
+            content="Minh | Frontend Developer & Web Designer"
+          />
+          <meta
+            property="og:description"
+            content="I create elevating digital experiences that inspire and connect with people through design and development"
+          />
+          <meta
+            property="og:image"
+            content="https://www.sogaiart.com/opengraph-image.png?7da4453306bf765e"
+          />
+          <meta property="og:url" content="https://liiana.vercel.app/" />
+        </Helmet>
+      </div>
 
       <BrowserRouter>
         <Routes>
