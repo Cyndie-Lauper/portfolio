20781a25e47991ee1717f4ce17cf4e4b9d005bcc
Minh <elevenine00@gmail.com>
2024-07-10 13:17:16 +0700

    add preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/HomePage";
 import Success from "./pages/SuccessPage";
+import { useEffect, useState } from "react";
+import { AnimatePresence } from "framer-motion";
+import Preloader from "./components/Preloader/Preloader";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+      setTimeout(() => {
+        setIsLoading(false);
+        // document.body.style.cursor = "default";
+        window.scrollTo(0, 0);
+      }, 2000);
+    });
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/success" element={<Success />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <AnimatePresence mode="wait">
+        {isLoading && <Preloader />}
+      </AnimatePresence>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/success" element={<Success />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 
