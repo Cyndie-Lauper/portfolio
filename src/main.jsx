a082652755a4d191c48b4b62f0fc22a090e3ff57
Minh <elevenine00@gmail.com>
2024-09-20 15:04:36 +0700

    fix: bug

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <Cursor /> */}
+    <Cursor />
     <App />
   </React.StrictMode>
 );
